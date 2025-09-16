084eba767b49874ff3761389f80ca1623158f211
shrutipatil-19 <shrupatil1319@gmail.com>
2025-09-16 16:43:30 +0530

    can directory

--- resources/views/project_management/partial/sidebar.blade.php
+++ resources/views/project_management/partial/sidebar.blade.php
@@ -9,7 +9,6 @@
                  <span class="menu-title">Dashboard</span>
              </a>
          </li>
-
          <li class="nav-item">
              <a class="nav-link" data-toggle="collapse" href="#ui-basic2" aria-expanded="false" aria-controls="ui-basic2">
                  <span class="icon-bg"><i class="mdi mdi-crosshairs-gps menu-icon"></i></span>
@@ -42,7 +41,7 @@
          <li class="nav-item">
              <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                  <span class="icon-bg"><i class="mdi mdi-crosshairs-gps menu-icon"></i></span>
-                 <span class="menu-title">Add Project</span>
+                 <span class="menu-title"> Add Project</span>
                  <i class="menu-arrow"></i>
              </a>
              <div class="collapse" id="ui-basic">
